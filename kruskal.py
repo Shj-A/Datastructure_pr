3adeff21e008679c55183378182c0b1cefb95650
Heejun SHin <shin970421@gmail.com>
2022-09-19 22:01:21 +0900

    Kruscal algorithm

--- kruskal.py
+++ kruskal.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 class Graph():
     def __init__(self, size):
         self.SIZE = size
@@ -5,6 +7,22 @@ class Graph():
 
 namearr = ["춘천","서울",'속초','대전','광주','부산']
 춘천,서울,속초,대전,광주,부산 = 0,1,2,3,4,5
+
+def find(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    rootA = find(parent, a)
+    rootB = find(parent, b)
+
+    if rootA < rootB:
+        parent[rootB] = rootA
+    else:
+        parent[rootA] = rootB
+
 
 #그래프 구성
 gsize = 6
@@ -18,4 +36,33 @@ G1.graph[부산][대전] = 30; G1.graph[부산][광주] = 25
 
 print(G1.graph)
 
-#
+#edge list
+tempary = []
+for i in range(gsize):
+    for k in range(gsize):
+        if G1.graph[i][k] != 0 :
+            tempary.append([G1.graph[i][k],i,k])
+
+tempary = sorted(tempary,key=itemgetter(0))
+edgelist = []
+for i in range(0,len(tempary),2) :
+    edgelist.append(tempary[i])
+print(edgelist)
+
+#parent list
+parent = [0]*gsize
+for i in range(gsize):
+    parent[i] = i
+
+
+res = []
+for edge in edgelist:
+    cost, start, end = edge
+    if find(parent, start) != find(parent,end):
+        union(parent,start,end)
+        info = [cost, namearr[start], namearr[end]]
+        res.append(info)
+    if len(res) == gsize -1:
+        break
+
+print(res)
